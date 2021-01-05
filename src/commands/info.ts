0af2ed2da6fec99f7082f2239f471c10345c67ad
Ren Ross <briross@udel.edu>
2021-01-05 15:02:13 -0500

    added info command

---
+++ src/commands/info.ts
@@ -0,0 +1,24 @@
+import { EmbedField, Message, MessageEmbed } from 'discord.js';
+import { SageClient } from '@lib/types/SageClient';
+import { BOT, PREFIX } from '@root/config';
+
+export const decription = `Provides information about ${BOT.NAME}.`;
+export const usage = '[command]';
+export const extendedHelp = '[...]';
+
+export async function run(msg: Message, [cmd]: [string]): Promise<Message> {
+	const info = 
+`Welcome to ${BOT.NAME}! A wonderful, magical bot that has been custom-coded to assist you while you use this discord server.
+	 
+Some features of ${BOT.NAME} include: 
+	• :man_mage:  Self-assignable roles
+	• :eyes:  A word restriced list to catch a limited amount of rule-breaking terms
+	• :alarm_clock:  Reminders in case — OH MY GOD IT\'S BURNING
+	• :fire:  And many more! (use s;help for the full list of commands)
+
+Our friend ${BOT.NAME} is actively maintained by Ben Segal, Josh Lyon, and Ren Ross. 
+Please let any of us know if you have any issues! We try to fix bugs as soon as possible and are still adding new features.
+
+If you\'re interested in how ${BOT.NAME} works, you can check the code out at <https://github.com/ud-cis-discord/bot>.`;
+	return msg.channel.send(info);
+}
