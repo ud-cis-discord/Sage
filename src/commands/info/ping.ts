7c6a85a2a56b20e282f357f8095f1721b2572f42
BenSegal855 <bensegal@udel.edu>
2021-01-05 10:41:19 -0500

    fix: run not fun

--- src/commands/info/ping.ts
+++ src/commands/info/ping.ts
@@ -3,7 +3,7 @@ import prettyMilliseconds from 'pretty-ms';
 
 export const decription = 'Runs a connection test to Discord';
 
-export async function fun(msg: Message): Promise<Message> {
+export async function run(msg: Message): Promise<Message> {
 	const responce = await msg.channel.send('Ping?');
 	return responce.edit(`Pong! Round trip took ${prettyMilliseconds(responce.createdTimestamp - msg.createdTimestamp)}, REST ping ${msg.client.ws.ping}ms.`);
 }
