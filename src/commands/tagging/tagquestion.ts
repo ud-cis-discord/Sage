5b468447e0e80883443f843a5dab1ec51bbde163
BenSegal855 <bensegal@udel.edu>
2021-01-06 21:37:58 -0500

    feat: completed tagquestion command
    Co-authored-by: Ren <renross@users.noreply.github.com>
    Co-authored-by: Josh Lyon <joshlyon@udel.edu>

--- src/commands/tagging/tagquestion.ts
+++ src/commands/tagging/tagquestion.ts
@@ -1,19 +1,43 @@
-import { Message } from 'discord.js';
+import { Message, TextChannel } from 'discord.js';
 import { SageClient } from '@lib/types/SageClient';
 import { Course } from '@lib/types/Course';
+import { Question } from '@root/src/lib/types/Question';
 
 export const description = 'Tags the specified question using the given assignemnt ID, for the';
 export const usage = '<messageLink>|<courseID>|<assignmentID>';
-export const aliases = ['tagq'];
+export const aliases = ['tagq', 'tag'];
 
 // never assume that students are not dumb
 
-/* export async function run(msg: Message, [link, course, assignment]: [string, string, string]): Promise<Message> {
+export async function run(msg: Message, [messageLink, courseId, assignmentId]: [string, string, string]): Promise<Message> {
+	const bot = msg.client as SageClient;
+	const entry = await bot.mongo.collection('questions').findOne({ link: messageLink, course: courseId, assignment: assignmentId });
 
-} */
+	if (entry) {
+		return msg.channel.send(`That message has already been tagged for ${assignmentId}`);
+	}
+
+	const [guildId, channelId, messageId] = messageLink.match(/(\d)+/g);
+	const channel = bot.guilds.cache.get(guildId).channels.cache.get(channelId) as TextChannel;
+	const question = await channel.messages.fetch(messageId);
+
+	if (!question) {
+		return msg.channel.send('I couldnt find a message with that message link.');
+	}
+
+	const newQuestion: Question = {
+		link: messageLink,
+		course: courseId,
+		assignment: assignmentId,
+		header: question.cleanContent.length < 200 ? question.cleanContent : `${question.cleanContent.slice(0, 200)}...`
+	};
+
+	bot.mongo.collection('questions').insertOne(newQuestion);
+	msg.channel.send('Added that message to the database.');
+}
 
 export async function argParser(msg: Message, input: string): Promise<[string, string, string]> {
-	const [link, course, assignment] = input.split('|');
+	const [link, course, assignment] = input.split('|').map(arg => arg.trim());
 	if (!link || !course || !assignment) {
 		throw `Usage: ${usage}`;
 	}
@@ -22,17 +46,18 @@ export async function argParser(msg: Message, input: string): Promise<[string, s
 
 	const newLink = link.replace('canary.', '');
 	const match = newLink.match(/https:\/\/discord\.com\/channels(\/(\d)+){3}/);
-	if (!match[0]) {
-		throw `<${newLink}> is not a valid Discord message link.`;
+
+	if (!match) {
+		throw `**${newLink}** is not a valid Discord message link.`;
 	}
 
 	const entry: Course = await bot.mongo.collection('courses').findOne({ name: course });
 	if (!entry) {
-		throw `Could not find course: ${course}`;
+		throw `Could not find course: **${course}**`;
 	}
 
 	if (!entry.assignments.includes(assignment)) {
-		throw `Could not find assignment: ${assignment} in course: ${course}`;
+		throw `Could not find assignment **${assignment}** in course: **${course}**.\n${course} curently has these assignments:\n\`${entry.assignments.join('`, `')}\``;
 	}
 
 	return [match[0], course, assignment];
