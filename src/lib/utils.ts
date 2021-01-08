22bb90edb9f7496180a0a29e200059a763dee7c3
BenSegal855 <bensegal@udel.edu>
2021-01-07 20:15:46 -0500

    tweak: sendToHastebin is now a util function

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,7 +1,15 @@
+import fetch from 'node-fetch';
 import { Command } from './types/Command';
 import { SageClient } from './types/SageClient';
 
 export function getCommand(bot: SageClient, cmd: string): Command {
 	cmd = cmd.toLowerCase();
 	return bot.commands.get(cmd) || bot.commands.find(command => command.aliases && command.aliases.includes(cmd));
+}
+
+export async function sendToHastebin(input: string, filetype = 'txt'): Promise<string> {
+	if (input.length < 1900) return input;
+
+	const res = await fetch('https://hastebin.com/documents', { method: 'POST', body: input }).then(r => r.json());
+	return `Result too long for Discord, uploaded to hastebin: <https://hastebin.com/${res.key}.${filetype}>`;
 }
