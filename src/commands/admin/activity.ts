03f8b79666b09cc9e679b858c71062fc304ac949
joshualyon7 <joshlyon@udel.edu>
2021-01-08 00:43:00 -0500

    feat: implemented status command (figuring out kinks)

--- src/commands/admin/activity.ts
+++ src/commands/admin/activity.ts
@@ -3,7 +3,7 @@ import { ActivityType, Message, Team } from 'discord.js';
 import { BOT } from '@root/config';
 
 export const description = `Sets ${BOT.NAME}'s activity to the given type and content`;
-export const usage = '<type|content>';
+export const usage = '<type>|<content>';
 
 export async function permissions(msg: Message): Promise<boolean> {
 	const team = (await msg.client.fetchApplication()).owner as Team;
@@ -18,11 +18,15 @@ export async function run(msg: Message, [type, content]: [ActivityType, string])
 
 export function argParser(msg: Message, input: string): [string, string] {
 	const [type, content] = input.split('|').map(arg => arg.trim());
+	if (!type || !content) {
+		throw `Usage: ${usage}`;
+	}
+
 	const upperType = type.toUpperCase();
 	const activities = ['PLAYING', 'STREAMING', 'LISTENING', 'WATCHING', 'COMPETING'];
 
 	if (!activities.includes(upperType)) {
-		throw `Invalid activity type ${type}, choose one of ${activities}.`;
+		throw `Invalid activity type ${type}, choose one of ${activities.map(a => a.toLowerCase()).join(', ')}.`;
 	}
 
 	return [upperType, content];
---
+++ src/commands/admin/status.ts
@@ -0,0 +1,31 @@
+import { BOT } from '@root/config';
+import { SageClient } from '@lib/types/SageClient';
+import { Message, Team } from 'discord.js';
+
+export const description = `Sets ${BOT.NAME}'s status.`;
+export const usage = '<online|idle|dnd|invisible>';
+
+export async function permissions(msg: Message): Promise<boolean> {
+	const team = (await msg.client.fetchApplication()).owner as Team;
+	return team.members.has(msg.author.id);
+}
+export async function run(msg: Message, [status]: ['online' | 'idle' | 'dnd' | 'invisible']): Promise<Message> {
+	const bot = msg.client as SageClient;
+	await bot.user.setStatus(status);
+
+	return msg.channel.send(`Set ${BOT.NAME}'s status to ${status}`);
+}
+
+export function argParser(msg: Message, input: string): [string] {
+	if (input === '') {
+		throw `Usage: ${usage}`;
+	}
+
+	const validStatuses = ['online', 'idle', 'dnd', 'invisible'];
+
+	if (!validStatuses.includes(input = input.trim())) {
+		throw `invalid status ${input}. Status must be one of ${validStatuses.join(', ')}`;
+	}
+
+	return [input];
+}
