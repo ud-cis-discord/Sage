f04dac71992b0f274b261ed30b252d15c70af5b1
Ren Ross <briross@udel.edu>
2021-01-09 14:57:41 -0500

    tweaked line 13

--- src/commands/check.ts
+++ src/commands/check.ts
@@ -10,12 +10,12 @@ export const aliases = ['count'];
 export async function run(msg: Message, [here]: [string]): Promise<void> {
 	const user: SageUser = await msg.member.client.mongo.collection('users').findOne({ discordId: msg.member.id });
 
-	if (here) {
-		msg.channel.send(`You have sent ${user.count} message${user.count === 1 ? '' : 's'}`);
+	if (here === 'here') {
+		msg.channel.send(`You have sent ${user.count} message${user.count === 1 ? '' : 's'}.`);
 	} else {
-		msg.author.send(`You have sent ${user.count} message${user.count === 1 ? '' : 's'}`)
-			.then(() => { if (msg.channel.type !== 'dm') msg.channel.send('Your message count has been sent to your DMs'); })
-			.catch(() => msg.channel.send('I couldnt send you a DM. Please enable DMs and try again'));
+		msg.author.send(`You have sent ${user.count} message${user.count === 1 ? '' : 's'}.`)
+			.then(() => { if (msg.channel.type !== 'dm') msg.channel.send(`Your message count has been sent to your DMs.`); })
+			.catch(() => msg.channel.send(`I couldn't send you a DM. Please enable DMs and try again.`));
 	}
 	return;
 }
