0b6d42dbe13a3a36a06824a1c014214abebc15d8
Ren Ross <briross@udel.edu>
2021-01-08 23:36:19 -0500

    added check cmd

---
+++ src/commands/check.ts
@@ -0,0 +1,21 @@
+import { Message, GuildMember } from 'discord.js';
+import { SageUser } from '@lib/types/SageUser';
+
+export const description = `Displays the users current message count. 
+If the word 'here' is used as an argument, the message count will be 
+sent in the same channel that the command was used in (rather than the user's dms).`;
+export const usage = '[here]';
+export const aliases = ['count'];
+
+export async function run(msg: Message, [here]: [string]): Promise<void> {
+	const user: SageUser = await msg.member.client.mongo.collection('users').findOne({ discordId: msg.member.id });
+
+	if (here) {
+		msg.channel.send(`You have sent ${user.count} message${user.count === 1 ? '' : 's'}`);
+	} else {
+		msg.author.send(`You have sent ${user.count} message${user.count === 1 ? '' : 's'}`, { split: { char: '\n' } })
+			.then(() => { if (msg.channel.type !== 'dm') msg.channel.send('Your message count has been sent to your DMss'); })
+			.catch(() => msg.channel.send('I couldnt send you a DM. Please enable DMs and try again'));
+	}
+	return;
+}
