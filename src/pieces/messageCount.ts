73199ec248853b5c9245228beecae6f0301b9a4a
joshualyon7 <joshlyon@udel.edu>
2021-01-25 16:46:51 -0500

    feat: added powerUser role/handling

--- src/pieces/messageCount.ts
+++ src/pieces/messageCount.ts
@@ -7,6 +7,8 @@ import { SageUser } from '@lib/types/SageUser';
 const xpRatio = 1.25;
 const startingColor = 80;
 const greenIncrement = 8;
+const maxGreen = '00ff00';
+const maxLevel = 20;
 
 async function register(bot: Client): Promise<void> {
 	const errLog = await bot.channels.fetch(LOG.ERROR) as TextChannel;
@@ -31,21 +33,42 @@ async function register(bot: Client): Promise<void> {
 			entry.curExp = entry.levelExp = Math.floor(entry.levelExp * xpRatio);
 			entry.level++;
 			if (entry.levelPings) {
-				sendLevelDM(msg, entry);
+				sendLevelPing(msg, entry);
 			}
 			let addRole: Role;
-			if (!(addRole = msg.guild.roles.cache.find(r => r.name === `Level ${entry.level}`))) { // make a new level role if it doesn't exist
+			if (!(addRole = msg.guild.roles.cache.find(r => r.name === `Level ${entry.level}`))
+			&& entry.level <= maxLevel) { // make a new level role if it doesn't exist
 				addRole = await msg.guild.roles.create({
 					data: {
 						name: `Level ${entry.level}`,
 						color: createLevelHex(entry.level),
 						position: msg.guild.roles.cache.get(ROLES.VERIFIED).position + 1,
 						permissions: 0
+					},
+					reason: `${msg.author.username} is the first to get to Level ${entry.level}`
+				});
+			}
+
+			if (entry.level <= maxLevel) {
+				msg.member.roles.remove(msg.member.roles.cache.find(r => r.name.startsWith('Level')), `${msg.author.username} leveled up.`);
+				msg.member.roles.add(addRole, `${msg.author.username} leveled up.`);
+			}
+
+			if (entry.level > maxLevel
+				&& !(addRole = msg.guild.roles.cache.find(r => r.name === `Power User`))) {
+				addRole = await msg.guild.roles.create({
+					data: {
+						name: `Power User`,
+						color: maxGreen,
+						position: msg.guild.roles.cache.get(ROLES.VERIFIED).position + 1,
+						permissions: 0
 					}
 				});
 			}
-			msg.member.roles.remove(msg.member.roles.cache.find(r => r.name.startsWith('Level'))).catch(); // remove old level role
-			msg.member.roles.add(addRole);
+			if (entry.level > maxLevel && !msg.member.roles.cache.find(r => r.name === 'Power User')) {
+				msg.member.roles.remove(msg.member.roles.cache.find(r => r.name.startsWith('Level')), `${msg.author.username} leveled up.`);
+				msg.member.roles.add(addRole, `${msg.author.username} leveled up.`);
+			}
 		}
 
 		bot.mongo.collection(DB.USERS).updateOne(
@@ -59,22 +82,23 @@ async function register(bot: Client): Promise<void> {
 	});
 }
 
-async function sendLevelDM(msg: Message, user: SageUser): Promise<Message> {
+async function sendLevelPing(msg: Message, user: SageUser): Promise<Message> {
 	let embedText: string;
 	if (startingColor + (user.level * greenIncrement) >= 255 - greenIncrement) {
 		embedText = `Congratulations, you have advanced to level ${user.level}!
 		\nYou're about as green as you can get, but keep striving for higher levels to show off to your friends!`;
 	} else {
-		embedText = `Congratulations, you have advanced to level ${user.level}!\n Keep up the great work!`;
+		embedText = `Congratulations ${msg.author.username}, you have advanced to level ${user.level}!\n Keep up the great work!`;
 	}
 	const embed = new MessageEmbed()
-		.setThumbnail(msg.client.user.avatarURL())
+		.setThumbnail(msg.author.avatarURL())
 		.setTitle('<:stevepeace:746223639770431578> Level up!')
 		.setDescription(embedText)
 		.addField('XP to next level:', user.levelExp, true)
 		.setColor(createLevelHex(user.level));
 
-	return msg.author.send(embed).catch(() => msg.channel.send('I couldn\'t send you a DM. Please enable DMs and try again.'));
+	// eslint-disable-next-line no-extra-parens
+	return (msg.guild.channels.cache.get(LOG.SAGE) as TextChannel).send(`${msg.member}, you have leveled up!`, embed);
 }
 
 function createLevelHex(level: number): string {
