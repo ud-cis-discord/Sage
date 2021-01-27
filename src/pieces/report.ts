9a2451fe1d9218b0b95d3874ca602da15a520c87
BenSegal855 <bensegal@udel.edu>
2021-01-27 13:54:52 -0500

    feat: weekly report

---
+++ src/pieces/report.ts
@@ -0,0 +1,50 @@
+import { Client } from 'discord.js';
+import { schedule } from 'node-cron';
+import { Cursor } from 'mongodb';
+import nodemailer from 'nodemailer';
+import { SageUser } from '@lib/types/SageUser';
+import { DB, EMAIL } from '@root/config';
+import moment from 'moment';
+
+
+async function register(bot: Client): Promise<void> {
+	// 0 * * * SAT
+	schedule('* * * * *', () => {
+		handleCron(bot).catch(err => console.error(err));
+	}, {
+		timezone: 'America/New_York'
+	});
+}
+
+async function handleCron(bot: Client): Promise<void> {
+	const users: Cursor<SageUser> = bot.mongo.collection(DB.USERS).find();
+	let report = 'Email,Count\n';
+	users.forEach(user => {
+		report += `${user.email},${user.count}\n`;
+	});
+
+	const mailer = nodemailer.createTransport({
+		host: 'mail.udel.edu',
+		port: 25
+	});
+
+	mailer.sendMail({
+		from: EMAIL.SENDER,
+		replyTo: EMAIL.REPLY_TO,
+		bcc: EMAIL.REPORT_ADDRESSES,
+		subject: 'Discord weekly Report',
+		html: `<!doctype html>
+<html>
+	<p> Here is your weekly Discord participation report. </p>
+	<p> The <span style="color:#738ADB">Discord</span> Admin Team </p>
+</html>`,
+		attachments: [{
+			filename: `report${moment().format('M-D-YY_HH-mm-ss')}.csv`,
+			content: report
+		}]
+	});
+
+	bot.mongo.collection(DB.USERS).updateMany({}, { $set: { count: 0 } });
+}
+
+export default register;
