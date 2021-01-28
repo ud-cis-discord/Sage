faae8f0255a11c4afc0e72f89cb3bbc2b069d8a8
BenSegal855 <bensegal@udel.edu>
2021-01-27 19:00:41 -0500

    tweak: info updates

--- src/commands/info/info.ts
+++ src/commands/info/info.ts
@@ -1,5 +1,5 @@
 import { Message } from 'discord.js';
-import { BOT, MAINTAINERS } from '@root/config';
+import { BOT, MAINTAINERS, PREFIX } from '@root/config';
 
 export const description = `Provides information about ${BOT.NAME}.`;
 
@@ -9,11 +9,11 @@ export async function run(msg: Message): Promise<Message> {
 	 
 Some features of ${BOT.NAME} include: 
 	• :man_mage:  Self-assignable roles
-	• :eyes:  A word restriced list to catch a limited amount of rule-breaking terms
-	• :alarm_clock:  Reminders in case — OH MY GOD IT'S BURNING
-	• :fire:  And many more! (use s;help for the full list of commands)
+	• :ticket: Question tagging so you can easily find questions others have asked
+	• :ninja: Private and anonymous questions
+	• :fire:  And many more! (use ${PREFIX}help for the full list of commands)
 
-Our friend ${BOT.NAME} was origionaly created by Ben Segal, Josh Lyon, and Ren Ross and is is actively maintained by ${MAINTAINERS}. 
+Our friend ${BOT.NAME} was originally created by Ben Segal, Josh Lyon, and Ren Ross and is is actively maintained by ${MAINTAINERS}. 
 Please let any of us know if you have any issues! We try to fix bugs as soon as possible and are still adding new features.
 
 If you're interested in how ${BOT.NAME} works, you can check the code out at <https://github.com/ud-cis-discord/SageV2>.`;
