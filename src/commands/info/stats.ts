6a88e4270879b22aaa36dd646e1fb6921344f6ac
BenSegal855 <ben@segals.org>
2021-02-04 23:19:17 -0500

    tweak: Stats command now shows djs & sage versions

--- src/commands/info/stats.ts
+++ src/commands/info/stats.ts
@@ -1,5 +1,6 @@
-import { EmbedField, Message, MessageEmbed } from 'discord.js';
+import { EmbedField, Message, MessageEmbed, version as discordVersion } from 'discord.js';
 import prettyMilliseconds from 'pretty-ms';
+import { version as sageVersion } from '@root/package.json';
 import { BOT } from '@root/config';
 
 export const description = 'Displays info about Sage\'s current status';
@@ -8,11 +9,6 @@ export function run(msg: Message): Promise<Message> {
 	const fields: Array<EmbedField> = [];
 	const bot = msg.client;
 
-	fields.push({
-		name: 'Uptime',
-		value: prettyMilliseconds(bot.uptime),
-		inline: true
-	});
 	fields.push({
 		name: 'Users',
 		value: `${bot.users.cache.size}`,
@@ -29,14 +25,30 @@ export function run(msg: Message): Promise<Message> {
 		inline: true
 	});
 	fields.push({
+		name: 'Uptime',
+		value: prettyMilliseconds(bot.uptime),
+		inline: true
+	});
+	fields.push({
 		name: 'Memory Usage',
 		value: `${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} MB`,
+		inline: true
+	});
+	fields.push({
+		name: 'Sage Version',
+		value: `v${sageVersion}`,
+		inline: false
+	});
+	fields.push({
+		name: 'Discord.js Version',
+		value: `v${discordVersion}`,
 		inline: true
 	});
 
 	const embed = new MessageEmbed()
 		.setColor('DARK_GREEN')
 		.setAuthor(`${BOT.NAME} Stats`, bot.user.displayAvatarURL())
+		.setThumbnail(bot.user.displayAvatarURL())
 		.setTimestamp(Date.now())
 		.addFields(fields);
 
