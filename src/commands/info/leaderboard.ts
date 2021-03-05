04a181a911add9ce8adf79f5f453a874b13c4f51
joshualyon7 <joshlyon@udel.edu>
2021-03-05 18:54:34 -0500

    tweak: more work on leaderboard

--- src/commands/info/leaderboard.ts
+++ src/commands/info/leaderboard.ts
@@ -10,29 +10,36 @@ export const aliases = ['rank', 'leader'];
 export async function run(msg: Message, [page]: [number]): Promise<Message> {
 	msg.guild.members.fetch();
 
+	// eslint-disable-next-line no-extra-parens
 	const users: Array<SageUser> = (await msg.client.mongo.collection('users').find().toArray() as Array<SageUser>)
-		.sort((ua, ub) => ua.level - ub.level !== 0 ? ua.level - ub.level : ua.curExp - ub.curExp); // filter on level first, then remaining xp
+		.sort((ua, ub) => ua.level - ub.level !== 0 ? ua.level > ub.level ? -1 : 1 : ua.curExp > ub.curExp ? -1 : 1); // filter on level first, then remaining xp
 
-	page = page || 1;
+	const start = (page * 10) - 10;
+	const end = page * 10 > users.length ? null : page * 10;
+	console.log(users[11].email);
 
-	const displUsers = users.slice((page * 10) - 10, page * 10);
+	const displUsers = users.slice(start, end);
+
+	let content = '';
+	displUsers.forEach(user => {
+		const rank = displUsers.indexOf(user) + 1 + ((page - 1) * 10);
+		const name = msg.guild.members.cache.get(user.discordId).displayName;
+		const { level } = user;
+		const exp = user.levelExp - user.curExp;
+		content += `**${rank}:** ${name} - Level ${level}, ${exp}exp\n`;
+	});
 
 	const embed = new MessageEmbed()
 		.setAuthor('UD CIS Discord Leaderboard')
-		.setTitle(`Showing`)
-		.setColor('green')
-		.addFields([
-			{ name: 'Rank', value: Array.from([...Array(10).keys()]).map(ind => ind * page), inline: true },
-			{ name: 'User', value: displUsers.map(user => msg.guild.members.cache.get(user.discordId).displayName).join(' '), inline: true },
-			{ name: 'Level', value: displUsers.map(user => user.level) },
-			{ name: 'Exp', value: displUsers.map(user => user.levelExp - user.curExp), inline: true }
-		]);
+		.setFooter(`Showing page ${page} (${start + 1} - ${end || users.length})`)
+		.setColor('GREEN')
+		.setDescription(content);
 
 	return msg.channel.send(embed);
 }
 
 export function argParser(msg: Message, input: string): Array<number | null> {
-	const page = parseInt(input);
+	const page = parseInt(input) || 1;
 
 	if (page < 1) throw 'Enter a number greater than 1';
 
