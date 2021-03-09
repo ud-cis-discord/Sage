e4b21079e11ec3f79f02bac4ff4c9f5f5b1d9610
joshualyon7 <joshlyon@udel.edu>
2021-03-09 10:53:51 -0500

    feat: finished leaderboard (closes #112)

--- src/commands/info/leaderboard.ts
+++ src/commands/info/leaderboard.ts
@@ -12,13 +12,15 @@ export async function run(msg: Message, [page]: [number]): Promise<Message> {
 
 	// eslint-disable-next-line no-extra-parens
 	const users: Array<SageUser> = (await msg.client.mongo.collection('users').find().toArray() as Array<SageUser>)
-		.sort((ua, ub) => ua.level - ub.level !== 0 ? ua.level > ub.level ? -1 : 1 : ua.curExp > ub.curExp ? -1 : 1); // filter on level first, then remaining xp
+		.sort((ua, ub) => ua.level - ub.level !== 0 ? ua.level > ub.level ? -1 : 1 : ua.curExp < ub.curExp ? -1 : 1); // filter on level first, then remaining xp
 
+	page = page * 10 > users.length ? Math.floor(users.length / 10) + 1 : page;
 	const start = (page * 10) - 10;
-	const end = page * 10 > users.length ? null : page * 10;
-	console.log(users[11].email);
+	const end = page * 10 > users.length ? undefined : page * 10;
 
 	const displUsers = users.slice(start, end);
+
+	const dbAuthor = users.find(user => msg.author.id === user.discordId);
 
 	let content = '';
 	displUsers.forEach(user => {
@@ -26,13 +28,22 @@ export async function run(msg: Message, [page]: [number]): Promise<Message> {
 		const name = msg.guild.members.cache.get(user.discordId).displayName;
 		const { level } = user;
 		const exp = user.levelExp - user.curExp;
-		content += `**${rank}:** ${name} - Level ${level}, ${exp}exp\n`;
+		content += `**${rank}:** ${user === dbAuthor ? `**${name}**` : name} - Level ${level}, ${exp} exp\n`;
 	});
 
+	if (!displUsers.find(user => user.discordId === msg.author.id)) {
+		const rank = users.indexOf(dbAuthor) + 1;
+		const name = msg.member.displayName;
+		const { level } = dbAuthor;
+		const exp = dbAuthor.levelExp - dbAuthor.curExp;
+		content = rank - 1 < start
+			? `**${rank}:** ${name} - Level ${level}, ${exp} exp \n\n${content}` : `${content}\n**${rank}:** ${name} - Level ${level}, ${exp} exp\n`;
+	}
+
 	const embed = new MessageEmbed()
-		.setAuthor('UD CIS Discord Leaderboard')
+		.setTitle('UD CIS Discord Leaderboard')
 		.setFooter(`Showing page ${page} (${start + 1} - ${end || users.length})`)
-		.setColor('GREEN')
+		.setColor(msg.guild.members.cache.get(displUsers[0].discordId).displayHexColor)
 		.setDescription(content);
 
 	return msg.channel.send(embed);
