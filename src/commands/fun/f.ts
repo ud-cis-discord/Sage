232678c3b7f085307e945da5d2f40bcc50c6241c
BenSegal855 <ben@segals.org>
2021-05-05 11:36:43 -0400

    fix: f command absolute path closes #180

--- src/commands/fun/f.ts
+++ src/commands/fun/f.ts
@@ -4,5 +4,8 @@ export const description = 'Press F to pay respects';
 export const aliases = ['respect'];
 
 export function run(msg: Message): Promise<Message> {
-	return msg.channel.send({ files: [{ attachment: './assets/images/f.png', name: 'pay_respects.png' }] });
+	return msg.channel.send({ files: [{
+		attachment: `${__dirname}../../../../../assets/images/f.png`,
+		name: 'pay_respects.png'
+	}] });
 }
