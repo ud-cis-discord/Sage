c45a686b1509491b963daad1039a66f45dba3ab1
Josh Lyon <joshlyon@udel.edu>
2021-05-09 17:37:14 -0400

    issue command now has description option (closes #181)

--- src/commands/admin/issue.ts
+++ src/commands/admin/issue.ts
@@ -15,14 +15,14 @@ export default class extends Command {
 		return adminPerms(msg);
 	}
 
-	async run(msg: Message, [title, project, labels, milestone]: [string, string, string[], string]): Promise<Message | void> {
+	async run(msg: Message, [title, project, labels, milestone, body]: [string, string, string[], string, string]): Promise<Message | void> {
 		const newIssue = await msg.client.octokit.issues.create({
 			owner: 'ud-cis-discord',
 			repo: project,
 			title: title,
 			milestone: milestone || undefined,
 			labels: labels,
-			body: `<sub>Created by ${msg.member.displayName} via ${BOT.NAME}</sub>`
+			body: `${body}\n\n<sub>Created by ${msg.member.displayName} via ${BOT.NAME}</sub>`
 		}).catch(response => {
 			console.log(response);
 			let errormsg = '';
@@ -51,6 +51,7 @@ export default class extends Command {
 		let project = 'SageV2';
 		let labels = [];
 		let milestone = '';
+		let body = '';
 
 		if (splitArgs.find(str => str.includes('project'))) {
 			[, project] = splitArgs.find(str => str.includes('project')).split('=');
@@ -67,7 +68,15 @@ export default class extends Command {
 			[, milestone] = splitArgs.find(str => str.includes('milestone')).split('=');
 			if (!milestone) throw `Invalid format for milestone name, more help:\n${this.extendedHelp}`;
 		}
-		return [title, project, labels, milestone];
+
+		if (splitArgs.find(str => str.includes('body'))) {
+			[, body] = splitArgs.find(str => str.includes('body')).split('=');
+			if (!body) throw `Invalid format for body, more help:\n${this.extendedHelp}`;
+		} else if (splitArgs.find(str => str.includes('description'))) {
+			[, body] = splitArgs.find(str => str.includes('description')).split('=');
+			if (!body) throw `Invalid format for description, more help:\n${this.extendedHelp}`;
+		}
+		return [title, project, labels, milestone, body];
 	}
 
 }
