7eb7acab325e73a88cd8131ab0393b3a8b97584f
Ren <60983762+renross@users.noreply.github.com>
2021-09-17 00:58:30 -0400

    removed code to delete message

--- src/commands/fun/coinflip.ts
+++ src/commands/fun/coinflip.ts
@@ -13,15 +13,17 @@ export default class extends Command {
 
 	async run(msg: Message) {
 		const coinFlip = await msg.channel.send('flipping...');
+		/*
 		const coinImage = await msg.channel.send({ files: [{
 			attachment: `${__dirname}../../../../../assets/images/coinflip.gif`,
 			name: `coin_flip.gif`
 		}] });
+		*/
 
 		const result = COIN_FLIP[Math.floor(Math.random() * COIN_FLIP.length)];
 
 		setTimeout(() => {
-			coinImage.delete();
+			//	coinImage.delete();
 			if (result == COIN_FLIP[0]) {
 				coinFlip.edit({ files: [{
 					attachment: `${__dirname}../../../../../assets/images/steve_heads.png`,
