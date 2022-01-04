a366bcef9d36e7aab079301272299dafb0c9a193
Josh Lyon <joshlyon@udel.edu>
2022-01-04 13:09:21 -0500

    changed ping to reply with anti-pong

--- src/commands/info/ping.ts
+++ src/commands/info/ping.ts
@@ -8,7 +8,7 @@ export default class extends Command {
 
 	async run(msg: Message): Promise<Message> {
 		const response = await msg.channel.send('Ping?');
-		return response.edit(`Pong! Round trip took ${prettyMilliseconds(response.createdTimestamp - msg.createdTimestamp)}, REST ping ${msg.client.ws.ping}ms.`);
+		return response.edit(`Anti-pong! Round trip took ${prettyMilliseconds(response.createdTimestamp - msg.createdTimestamp)}, REST ping ${msg.client.ws.ping}ms.`);
 	}
 
 }
