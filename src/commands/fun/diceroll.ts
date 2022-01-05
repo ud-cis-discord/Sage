c9f75087bfd2257dad96dca3c7065a9f5165eb66
sbrugel <sbrugel62@gmail.com>
2022-01-04 21:26:44 -0500

    fix: remove unneeded parentheses, desc uses default range const

--- src/commands/fun/diceroll.ts
+++ src/commands/fun/diceroll.ts
@@ -1,9 +1,10 @@
 import { Message } from 'discord.js';
 import { Command } from '@lib/types/Command';
 
+const DEFAULT_RANGE = [1, 6];
 export default class extends Command {
 
-	description = 'Get a random number within a user-specified range (min and max inclusive). If no range is specified by the user, defaults to a range from 1 to 6. Also works for negative numbers!';
+	description = `Get a random number within a user-specified range (min and max inclusive). If no range is specified by the user, defaults to a range from ${DEFAULT_RANGE[0]} to ${DEFAULT_RANGE[1]}.`;
 	usage = '[min #] | [max #]';
 	aliases = ['random', 'rand'];
 
@@ -13,20 +14,19 @@ export default class extends Command {
 
 	argParser(_msg: Message, input: string): Array<number> {
 		const [minimum, maximum, ...rest] = input.split('|').map(arg => arg.trim());
-		const DEFAULT_RANGE = [1, 6];
 		if (!input) {
 			return DEFAULT_RANGE;
 		} else {
 			if (rest.length > 0) {
-				throw ('You have supplied too many arguments (maximum is 2).');
+				throw 'You have supplied too many arguments (maximum is 2).';
 			}
 
 			if (!maximum) {
-				throw ('If you provide a minimum, you must also provide a maximum.');
+				throw 'If you provide a minimum, you must also provide a maximum.';
 			} else if (Number(maximum) < Number(minimum)) {
-				throw ('Your minimum must be less than your maximum.');
+				throw 'Your minimum must be less than your maximum.';
 			} else if (isNaN(Number(maximum)) || isNaN(Number(minimum))) {
-				throw ('One or both of your inputs was not a number.');
+				throw 'One or both of your inputs was not a number.';
 			}
 			return [Number(minimum), Number(maximum)];
 		}
