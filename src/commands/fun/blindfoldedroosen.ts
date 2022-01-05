57545d81deed3c4d9482a41b9b152a7295d9ad0a
Blade Tyrrell <bladetyr@udel.edu>
2022-01-04 20:07:35 -0500

    added blindfoldedroosen command

---
+++ src/commands/fun/blindfoldedroosen.ts
@@ -0,0 +1,20 @@
+import { Message } from 'discord.js';
+import { Command } from '@lib/types/Command';
+
+export default class extends Command {
+
+	description = `You've been challenged to a sword fight. However, your opponent, Professor Roosen, has decided to wear a blindfold. Feeling lucky?`;
+	usage = '<blindfoldedroosen>';
+	aliases = ['br', 'blindfoldedroosen'];
+
+	run(msg: Message): Promise<Message> {
+		const chosenSword = Math.floor(Math.random() * 6);
+		// 5 is a random number I chose to be the blindfolded Roosen, no other meaning
+		if (chosenSword === 5) {
+			return msg.channel.send('Ooooooooooooh... ouch! Roosen has accidentally killed you! You lose.');
+		} else {
+			return msg.channel.send('You\'ve won the fight against blindfolded Roosen. You live another day!');
+		}
+	}
+
+}
