f192676549f6b166210685095441c0fae4f0e65a
sbrugel <sbrugel62@gmail.com>
2022-01-13 13:26:18 -0500

    slashified 8ball/bfr/coinflip/diceroll/f/tif
    Also fixes usage since question is a required argument (previously it used square brackets, implying it was optional)
    
    Aliases removed as these do not work with slash commands (without multiple registrations)

--- src/commands/fun/blindfoldedroosen.ts
+++ src/commands/fun/blindfoldedroosen.ts
@@ -1,19 +1,29 @@
-import { Message } from 'discord.js';
+import { CommandInteraction, Message, MessageEmbed } from 'discord.js';
 import { Command } from '@lib/types/Command';
 
 export default class extends Command {
 
 	description = 'Challenge a blindfolded Prof. Roosen to a sword fight!';
 	extendedHelp = 'You\'ve been challenged to a sword fight. However, your opponent, Professor Roosen, has decided to wear a blindfold. Feeling lucky?';
-	aliases = ['br', 'bfr'];
 
-	run(msg: Message): Promise<Message> {
+	run(_msg: Message): Promise<void> { return; }
+
+	tempRun(interaction: CommandInteraction): Promise<void> {
 		const chosenSword = Math.floor(Math.random() * 6);
 		// 5 is a random number I chose to be the blindfolded Roosen, no other meaning
+		let responseEmbed: MessageEmbed;
 		if (chosenSword === 5) {
-			return msg.channel.send('Ooooooooooooh... ouch! Blindfolded Roosen has killed you! You lose.');
+			responseEmbed = new MessageEmbed()
+				.setColor('#ff0000')
+				.setTitle('Battle results')
+				.setDescription('Ooooooooooooh... ouch! Blindfolded Roosen has killed you! You lose.');
+			return interaction.reply({ embeds: [responseEmbed] });
 		} else {
-			return msg.channel.send('You\'ve won the fight against blindfolded Roosen. You live another day!');
+			responseEmbed = new MessageEmbed()
+				.setColor('#00ff00')
+				.setTitle('Battle results')
+				.setDescription('You\'ve won the fight against blindfolded Roosen. You live another day!');
+			return interaction.reply({ embeds: [responseEmbed] });
 		}
 	}
 
--- src/commands/fun/f.ts
+++ src/commands/fun/f.ts
@@ -1,16 +1,28 @@
-import { Message } from 'discord.js';
+import { ApplicationCommandOptionData, CommandInteraction, Message } from 'discord.js';
 import { Command } from '@lib/types/Command';
 
 export default class extends Command {
 
 	description = 'Press F to pay respects';
-	aliases = ['respect'];
+	options: ApplicationCommandOptionData[] = [
+		{
+			name: 'target',
+			description: '(Optional) The user to pay respects to',
+			type: 'USER',
+			required: false
+		}
+	]
 
-	run(msg: Message): Promise<Message> {
-		return msg.channel.send({ files: [{
+	run(_msg: Message): Promise<void> { return; }
+
+	tempRun(interaction: CommandInteraction): Promise<void> {
+		const replyContent = (interaction.options.get('target'))
+			? `${interaction.user.username} paid their respects to ${interaction.options.get('target').user.username}`
+			: `${interaction.user.username} paid their respects`;
+		return interaction.reply({ files: [{
 			attachment: `${__dirname}../../../../../assets/images/f.png`,
 			name: 'pay_respects.png'
-		}] });
+		}], content: replyContent });
 	}
 
 }
