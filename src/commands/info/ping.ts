2624507955b7c2b93fb9fa1e004629ce875e5093
Blade Tyrrell <bladetyr@udel.edu>
2022-01-12 20:06:09 -0500

    demolished run() inner body since it was uneeded

--- src/commands/info/ping.ts
+++ src/commands/info/ping.ts
@@ -7,9 +7,8 @@ export default class extends Command {
 	description = 'Runs a connection test to Discord';
 
 	// left in for the export default
-	async run(msg: Message): Promise<Message> {
-		const response = await msg.channel.send('Ping?');
-		return response.edit(`Pong! Round trip took ${prettyMilliseconds(response.createdTimestamp - msg.createdTimestamp)}, REST ping ${msg.client.ws.ping}ms.`);
+	async run(): Promise<void> {
+		return;
 	}
 
 	async tempRun(interaction: CommandInteraction): Promise<void> {
