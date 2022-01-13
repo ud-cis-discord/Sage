1261d8ac1944e33654065f70b68982b5b8c42558
sbrugel <sbrugel62@gmail.com>
2022-01-13 13:26:18 -0500

    slashified 8ball/bfr/coinflip/diceroll/f/tif
    Also fixes usage since question is a required argument (previously it used square brackets, implying it was optional)
    
    Aliases removed as these do not work with slash commands (without multiple registrations)

--- src/commands/fun/diceroll.ts
+++ src/commands/fun/diceroll.ts
@@ -1,25 +1,31 @@
-import { ApplicationCommandOptionData, ApplicationCommandPermissionData, CommandInteraction, Message } from 'discord.js';
+import { ApplicationCommandOptionData, ApplicationCommandPermissionData, CommandInteraction, Message, MessageEmbed } from 'discord.js';
 import { Command } from '@lib/types/Command';
 import { ROLES } from '@root/config';
 
 const DEFAULT_RANGE = [1, 6];
+const DEFAULT_ROLLS = 1;
 export default class extends Command {
 
-	description = `Get a random integer between \`minimum\` and \`maximum\`.`;
-	extendedHelp = `User specified minimum and maximum are inclusive. If no range is specified, defaults to a range from ${DEFAULT_RANGE[0]} to ${DEFAULT_RANGE[1]}.`;
-	usage = '[min #] | [max #]';
-	aliases = ['random', 'rand', 'dice', 'roll'];
+	description = `Get \`numdice\` random integers between \`minimum\` and \`maximum\`.`;
+	extendedHelp = `User specified minimum and maximum are inclusive. If no range is specified, defaults to one number ranging from ${DEFAULT_RANGE[0]} to ${DEFAULT_RANGE[1]}.`;
+	usage = '[min #] | [max #] | [numdice]';
 
 	options: ApplicationCommandOptionData[] = [
 		{
 			name: 'minimum',
-			description: 'minimum of the roll range',
+			description: `Minimum of the roll range (defaults to ${DEFAULT_RANGE[0]})`,
 			type: 'NUMBER',
 			required: false
 		},
 		{
 			name: 'maximum',
-			description: 'minimum of the roll range',
+			description: `Maximum of the roll range (defaults to ${DEFAULT_RANGE[1]})`,
+			type: 'NUMBER',
+			required: false
+		},
+		{
+			name: 'numdice',
+			description: `Number of dice to roll (1-10) (defaults to ${DEFAULT_ROLLS})`,
 			type: 'NUMBER',
 			required: false
 		}
@@ -28,29 +34,52 @@ export default class extends Command {
 	run(_msg: Message): Promise<void> { return; }
 
 	tempRun(interaction: CommandInteraction): Promise<void> {
-		const min = interaction.options.getNumber('minimum') || DEFAULT_RANGE[0];
-		const max = interaction.options.getNumber('maximum') || DEFAULT_RANGE[1];
-		return interaction.reply(`Your random number is ${Math.floor((Math.random() * (max - min + 1)) + min)}`);
-	}
+		let min = interaction.options.getNumber('minimum');
+		let max = interaction.options.getNumber('maximum');
+		const numRolls = interaction.options.getNumber('numdice') || DEFAULT_ROLLS;
 
-	argParser(_msg: Message, input: string): Array<number> {
-		const [minimum, maximum, ...rest] = input.split('|').map(arg => arg.trim());
-		if (!input) {
-			return DEFAULT_RANGE;
-		} else {
-			if (rest.length > 0) {
-				throw 'You have supplied too many arguments (maximum is 2).';
-			}
-
-			if (!maximum) {
-				throw 'If you provide a minimum, you must also provide a maximum.';
-			} else if (Number(maximum) < Number(minimum)) {
-				throw 'Your minimum must be less than your maximum.';
-			} else if (isNaN(Number(maximum)) || isNaN(Number(minimum))) {
-				throw 'One or both of your inputs was not a number.';
-			}
-			return [Number(minimum), Number(maximum)];
+		let responseEmbed: MessageEmbed;
+		if (!min) {
+			[min, max] = [DEFAULT_RANGE[0], DEFAULT_RANGE[1]];
+		} else if (!max && min) {
+			responseEmbed = new MessageEmbed()
+				.setColor('#ff0000')
+				.setTitle('Argument error')
+				.setDescription('If you provide a minimum, you must also provide a maximum.');
+			return interaction.reply({ embeds: [responseEmbed], ephemeral: true });
+		} else if (max < min) {
+			responseEmbed = new MessageEmbed()
+				.setColor('#ff0000')
+				.setTitle('Argument error')
+				.setDescription('Your maximum must be greater than your minimum.');
+			return interaction.reply({ embeds: [responseEmbed], ephemeral: true });
+		} else if (numRolls < 1 || numRolls > 10) {
+			responseEmbed = new MessageEmbed()
+				.setColor('#ff0000')
+				.setTitle('Argument error')
+				.setDescription('You can only roll between 1 and 10 dice.');
+			return interaction.reply({ embeds: [responseEmbed], ephemeral: true });
 		}
+
+		const results = [];
+		for (let i = 0; i < numRolls; i++) {
+			results.push(Math.floor((Math.random() * (max - min + 1)) + min));
+		}
+
+		const nums = results.join(', ');
+		let description;
+		if (results.length === 1) {
+			description = `Your random number is ${nums}.`;
+		} else {
+			description = `Your random numbers are ${nums}.`;
+		}
+
+		responseEmbed = new MessageEmbed()
+			.setColor(Math.floor(Math.random() * 16777215))
+			.setTitle('Random Integer Generator')
+			.setDescription(description)
+			.setFooter(`${interaction.user.username} rolled ${numRolls} dice ranging from ${min} to ${max}`);
+		return interaction.reply({ embeds: [responseEmbed] });
 	}
 
 }
