415b09ff3a1e6b70547e44e9468e195bdfc37389
Blade Tyrrell <bladetyr@udel.edu>
2022-01-12 19:42:03 -0500

    Slashified Ping (may need refactor)

--- src/commands/info/ping.ts
+++ src/commands/info/ping.ts
@@ -1,14 +1,21 @@
-import { Message } from 'discord.js';
-import prettyMilliseconds from 'pretty-ms';
+import { CommandInteraction, Message } from 'discord.js';
 import { Command } from '@lib/types/Command';
+import prettyMilliseconds from 'pretty-ms';
 
 export default class extends Command {
 
 	description = 'Runs a connection test to Discord';
 
+	// left in for the export default
 	async run(msg: Message): Promise<Message> {
 		const response = await msg.channel.send('Ping?');
 		return response.edit(`Pong! Round trip took ${prettyMilliseconds(response.createdTimestamp - msg.createdTimestamp)}, REST ping ${msg.client.ws.ping}ms.`);
+	}
+
+	async tempRun(interaction: CommandInteraction): Promise<void> {
+		const response = await interaction.channel.send('Ping?');
+		response.delete();
+		return interaction.reply(`Pong! Round trip took ${prettyMilliseconds(response.createdTimestamp - interaction.createdTimestamp)}, REST ping ${interaction.client.ws.ping}ms.`);
 	}
 
 }
