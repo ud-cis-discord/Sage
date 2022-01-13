2b2c2c318fbf2e303d08b49b2c2ee6e13dcdddb4
Josh Lyon <joshlyon@udel.edu>
2022-01-12 21:36:59 -0500

    Switched over Google command

--- src/commands/staff/google.ts
+++ src/commands/staff/google.ts
@@ -1,13 +1,40 @@
 import { staffPerms } from '@lib/permissions';
 import { Command } from '@lib/types/Command';
-import { BOT } from '@root/config';
-import { MessageEmbed, Message } from 'discord.js';
+import { BOT, ROLES } from '@root/config';
+import { MessageEmbed, Message, ApplicationCommandPermissionData, ApplicationCommandOptionData, CommandInteraction } from 'discord.js';
 
 export default class extends Command {
+
+	tempPermissions: ApplicationCommandPermissionData[] = [{
+		id: ROLES.STAFF,
+		permission: true,
+		type: 'ROLE'
+	}];
 
 	aliases = ['lmgt', 'lmg'];
 	description = `Have ${BOT.NAME} google something for someone`;
 	usage = '<query>';
+
+	options: ApplicationCommandOptionData[] = [
+		{
+			name: 'query',
+			type: 'STRING',
+			description: `What you'd like ${BOT.NAME} to Google for someone!`,
+			required: true
+		}
+	];
+
+	tempRun(interaction: CommandInteraction): Promise<void> {
+		const query = interaction.options.getString('query');
+		const formatted = query.replace(new RegExp(' ', 'g'), '+').replace('%', '%25');
+		const link = `https://letmegooglethat.com/?q=${formatted}`;
+		const embed = new MessageEmbed({
+			description: `[Let me Google that for you!](${link})`,
+			color: 'LUMINOUS_VIVID_PINK'
+		});
+		return interaction.reply({ embeds: [embed] });
+	}
+
 
 	async permissions(msg: Message): Promise<boolean> {
 		return staffPerms(msg);
