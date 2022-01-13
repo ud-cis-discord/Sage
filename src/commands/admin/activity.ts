459e4e9a0a07c9d53b1a12531b59fd9476bb970b
renross <briross@udel.edu>
2022-01-13 01:11:11 -0500

    activity.ts slashified still working on botmaster perms

--- src/commands/admin/activity.ts
+++ src/commands/admin/activity.ts
@@ -1,15 +1,44 @@
-import { ActivityType, Message } from 'discord.js';
+import { ActivityType, ApplicationCommandOptionData, ApplicationCommandPermissionData, CommandInteraction, Message } from 'discord.js';
 import { BOT, DB } from '@root/config';
-import { botMasterPerms } from '@lib/permissions';
+import { botMasterPerms, BOTMASTER_PERMS } from '@lib/permissions';
 import { Command } from '@lib/types/Command';
 
 export default class extends Command {
 
 	description = `Sets ${BOT.NAME}'s activity to the given type and content`;
 	usage = '<type>|<content>';
+	aliases = ['setactivity'];
+	tempPermissions: ApplicationCommandPermissionData[] = [BOTMASTER_PERMS];
+
+	options: ApplicationCommandOptionData[] = [
+		{
+			name: 'status',
+			description: 'The activity status (ex: PLAYING).',
+			type: 'STRING',
+			required: true
+		},
+		{
+			name: 'content',
+			description: 'The activity itself (ex: a game).',
+			type: 'STRING',
+			required: true
+		}
+	]
 
 	async permissions(msg: Message): Promise<boolean> {
-		return await botMasterPerms(msg);
+		return botMasterPerms(msg);
+	}
+
+	async tempRun(interaction: CommandInteraction): Promise<void> {
+		const bot = interaction.client;
+		const name = interaction.options.getString('content');
+		const type = interaction.options.getString('status');
+		bot.user.setActivity(name, { type });
+		bot.mongo.collection(DB.CLIENT_DATA).updateOne(
+			{ _id: bot.user.id },
+			{ $set: { status: { type, name } } },
+			{ upsert: true });
+		return interaction.reply(`Set ${BOT.NAME}'s activity to *${type} ${name}*`);
 	}
 
 	async run(msg: Message, [type, name]: [ActivityType, string]): Promise<Message> {
