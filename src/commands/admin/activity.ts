6c97f154670be6751a13652a562d38bd99687099
renross <briross@udel.edu>
2022-01-13 15:02:18 -0500

    limiting args

--- src/commands/admin/activity.ts
+++ src/commands/admin/activity.ts
@@ -1,45 +1,52 @@
 import { ActivityType, ApplicationCommandOptionData, ApplicationCommandPermissionData, CommandInteraction, Message } from 'discord.js';
 import { BOT, DB } from '@root/config';
-import { botMasterPerms, BOTMASTER_PERMS } from '@lib/permissions';
+import { BOTMASTER_PERMS } from '@lib/permissions';
 import { Command } from '@lib/types/Command';
 
+const args = ['PLAYING', 'STREAMING', 'LISTENING', 'WATCHING', 'COMPETING'];
 export default class extends Command {
 
-	description = `Sets ${BOT.NAME}'s activity to the given type and content`;
-	usage = '<type>|<content>';
-	aliases = ['setactivity'];
+	description = `Sets ${BOT.NAME}'s activity to the given status and content`;
+	usage = '<status>|<content>';
+	aliases = ['setactivity', 'act'];
 	tempPermissions: ApplicationCommandPermissionData[] = [BOTMASTER_PERMS];
+	//	botmaster perms
 
 	options: ApplicationCommandOptionData[] = [
 		{
 			name: 'status',
-			description: 'The activity status (ex: PLAYING).',
+			description: 'The activity status (ex: Playing).',
 			type: 'STRING',
-			required: true
+			required: true,
+			choices: args.map((arg) => ({
+				name: arg,
+				value: arg
+			}))
 		},
 		{
 			name: 'content',
-			description: 'The activity itself (ex: a game).',
+			description: 'The activity itself (ex: s;help).',
 			type: 'STRING',
 			required: true
 		}
 	]
 
-	async permissions(msg: Message): Promise<boolean> {
-		return botMasterPerms(msg);
-	}
-
 	async tempRun(interaction: CommandInteraction): Promise<void> {
 		const bot = interaction.client;
 		const content = interaction.options.getString('content');
 		const type = interaction.options.getString('status').toUpperCase() as ActivityType;
+
+		//	setting Sage's activity status in the guild
 		bot.user.setActivity(content, { type });
+		//	updating Sage's activity status in the database (so that it stays upon a restart)
 		bot.mongo.collection(DB.CLIENT_DATA).updateOne(
 			{ _id: bot.user.id },
 			{ $set: { status: { type, content } } },
 			{ upsert: true });
+
+		//	this is just formatting the reply string
 		const replyType = type[0] + type.slice(1).toLowerCase();
-		return interaction.reply(`Set ${BOT.NAME}'s activity to *${replyType} ${content}*`);
+		interaction.reply(`Set ${BOT.NAME}'s activity to *${replyType} ${content}*`);
 	}
 
 	async run(msg: Message, [type, name]: [ActivityType, string]): Promise<Message> {
