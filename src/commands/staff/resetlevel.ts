bdc3372890cbbad5be9432b12f8a8d5b783ad387
Josh Lyon <joshlyon@udel.edu>
2022-01-14 11:16:23 -0500

    resetlevel slashified

--- src/commands/staff/resetlevel.ts
+++ src/commands/staff/resetlevel.ts
@@ -1,5 +1,5 @@
-import { GuildMember, Message } from 'discord.js';
-import { staffPerms } from '@lib/permissions';
+import { ApplicationCommandOptionData, ApplicationCommandPermissionData, CommandInteraction, GuildMember, Message } from 'discord.js';
+import { ADMIN_PERMS, staffPerms, STAFF_PERMS } from '@lib/permissions';
 import { userParser } from '@lib/arguments';
 import { SageUser } from '@lib/types/SageUser';
 import { DatabaseError } from '@lib/types/errors';
@@ -14,6 +14,54 @@ export default class extends Command {
 	set their message count and a negative will subtract that from their total`;
 	runInDM = false;
 	aliases = ['reset'];
+
+	options: ApplicationCommandOptionData[] = [
+		{
+			name: 'user',
+			description: 'The user whose message count will be edited',
+			type: 'USER',
+			required: true
+		},
+		{
+			name: 'value',
+			description: 'value to use (positive to set, negative to subtract, none to set to 0)',
+			type: 'INTEGER',
+			required: false
+		}
+	];
+
+	async tempRun(interaction: CommandInteraction): Promise<void> {
+		const user = interaction.options.getUser('user');
+		const amount = interaction.options.getInteger('value') || 0;
+		const entry: SageUser = await interaction.client.mongo.collection(DB.USERS).findOne({ discordId: user.id });
+
+		if (!entry) {
+			throw new DatabaseError(`User ${user.username} (${user.id}) not in database`);
+		}
+
+		let retStr: string;
+
+		if (amount < 0) {
+			entry.count += amount;
+			if (entry.count < 0) {
+				entry.count = 0;
+				retStr = `Subtracted ${amount * -1} from ${user.username}'s message count (bottomed out at 0).`;
+			} else {
+				retStr = `Subtracted ${amount * -1} from ${user.username}'s message count.`;
+			}
+		} else {
+			entry.count = amount;
+			retStr = `Set ${user.username}'s message count to ${amount}.`;
+		}
+
+		await interaction.client.mongo.collection(DB.USERS).updateOne(
+			{ discordId: user.id },
+			{ $set: { count: entry.count } });
+
+		return interaction.reply({ content: retStr, ephemeral: true });
+	}
+
+	tempPermissions: ApplicationCommandPermissionData[] = [STAFF_PERMS, ADMIN_PERMS];
 
 	permissions(msg: Message): boolean {
 		return staffPerms(msg);
