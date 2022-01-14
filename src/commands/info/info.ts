a19f8a2b9a28835d761544f20e95ca713ab022f5
Blade Tyrrell <bladetyr@udel.edu>
2022-01-14 14:26:49 -0500

    slashified info, serverinfo, and stats

--- src/commands/info/info.ts
+++ src/commands/info/info.ts
@@ -1,4 +1,4 @@
-import { Message } from 'discord.js';
+import { CommandInteraction } from 'discord.js';
 import { BOT, MAINTAINERS, PREFIX } from '@root/config';
 import { Command } from '@lib/types/Command';
 
@@ -6,21 +6,26 @@ export default class extends Command {
 
 	description = `Provides information about ${BOT.NAME}.`;
 
-	async run(msg: Message): Promise<Message> {
+	async run(): Promise<void> {
+		return;
+	}
+
+	async tempRun(interaction: CommandInteraction): Promise<void> {
 		const info
-	= `Welcome to ${BOT.NAME}! A wonderful, magical bot that has been custom-coded to assist you while you use this discord server.
-		
-Some features of ${BOT.NAME} include: 
-	• :man_mage:  Self-assignable roles
-	• :ticket: Question tagging so you can easily find questions others have asked
-	• :ninja: Private and anonymous questions
-	• :fire:  And many more! (use ${PREFIX}help for the full list of commands)
-
-Our friend ${BOT.NAME} was originally created by Ben Segal, Josh Lyon, and Ren Ross and is is actively maintained by ${MAINTAINERS}. 
-Please let any of us know if you have any issues! We try to fix bugs as soon as possible and are still adding new features.
-
-If you're interested in how ${BOT.NAME} works, you can check the code out at <https://github.com/ud-cis-discord/SageV2>.`;
-		return msg.channel.send(info);
+		= `Welcome to ${BOT.NAME}! A wonderful, magical bot that has been custom-coded to assist you while you use this discord server.
+			
+	Some features of ${BOT.NAME} include: 
+		• :man_mage:  Self-assignable roles
+		• :ticket: Question tagging so you can easily find questions others have asked
+		• :ninja: Private and anonymous questions
+		• :fire:  And many more! (use ${PREFIX}help for the full list of commands)
+	
+	Our friend ${BOT.NAME} was originally created by Ben Segal, Josh Lyon, and Ren Ross and is is actively maintained by ${MAINTAINERS}.
+	 
+	Please let any of us know if you have any issues! We try to fix bugs as soon as possible and are still adding new features.
+	
+	If you're interested in how ${BOT.NAME} works, you can check the code out at <https://github.com/ud-cis-discord/SageV2>.`;
+		return interaction.reply(info);
 	}
 
 }
--- src/commands/info/serverinfo.ts
+++ src/commands/info/serverinfo.ts
@@ -1,32 +1,35 @@
-import { Message, MessageEmbed } from 'discord.js';
+import { CommandInteraction, MessageEmbed } from 'discord.js';
 import { Command } from '@lib/types/Command';
 
 export default class extends Command {
 
 	description = 'Provides information about the UDCIS discord server.';
-	aliases = ['serverstats'];
 	runInDM = false;
 
-	async run(msg: Message): Promise<Message> {
-		const membersWithRoles = msg.guild.members.cache.filter(m => m.roles.cache.size > 1).size;
-		const percentage = Math.floor((msg.guild.members.cache.filter(m => m.roles.cache.size > 1).size / msg.guild.memberCount) * 100);
+	async run(): Promise<void> {
+		return;
+	}
+
+	async tempRun(interaction: CommandInteraction): Promise<void> {
+		const membersWithRoles = interaction.guild.members.cache.filter(m => m.roles.cache.size > 1).size;
+		const percentage = Math.floor((interaction.guild.members.cache.filter(m => m.roles.cache.size > 1).size / interaction.guild.memberCount) * 100);
 
 		const embed = new MessageEmbed()
 			.addFields([
-				{ name: 'Total Members', value: msg.guild.memberCount.toString(), inline: true },
-				{ name: 'Humans', value: msg.guild.members.cache.filter(m => !m.user.bot).size.toString(), inline: true },
-				{ name: 'Bots', value: msg.guild.members.cache.filter(m => m.user.bot).size.toString(), inline: true },
-				{ name: 'Text Channels', value: msg.guild.channels.cache.filter(c => c.type === 'GUILD_TEXT').size.toString(), inline: true },
-				{ name: 'Voice Channels', value: msg.guild.channels.cache.filter(c => c.type === 'GUILD_VOICE').size.toString(), inline: true },
-				{ name: 'Roles', value: msg.guild.roles.cache.size.toString(), inline: true },
-				{ name: 'Emojis', value: msg.guild.emojis.cache.size.toString(), inline: true },
+				{ name: 'Total Members', value: interaction.guild.memberCount.toString(), inline: true },
+				{ name: 'Humans', value: interaction.guild.members.cache.filter(m => !m.user.bot).size.toString(), inline: true },
+				{ name: 'Bots', value: interaction.guild.members.cache.filter(m => m.user.bot).size.toString(), inline: true },
+				{ name: 'Text Channels', value: interaction.guild.channels.cache.filter(c => c.type === 'GUILD_TEXT').size.toString(), inline: true },
+				{ name: 'Voice Channels', value: interaction.guild.channels.cache.filter(c => c.type === 'GUILD_VOICE').size.toString(), inline: true },
+				{ name: 'Roles', value: interaction.guild.roles.cache.size.toString(), inline: true },
+				{ name: 'Emojis', value: interaction.guild.emojis.cache.size.toString(), inline: true },
 				{ name: 'Members with Roles', value: `${membersWithRoles} (${percentage}%)`, inline: true }
 			])
-			.setAuthor(msg.guild.name, msg.guild.iconURL())
+			.setAuthor(interaction.guild.name, interaction.guild.iconURL())
 			.setColor('DARK_VIVID_PINK')
 			.setTimestamp();
 
-		return msg.channel.send({ embeds: [embed] });
+		return interaction.reply({ embeds: [embed] });
 	}
 
 }
