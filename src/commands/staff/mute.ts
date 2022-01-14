1735c91788de8b0f67a3c60dc8d7f7f2569964d7
Josh Lyon <joshlyon@udel.edu>
2022-01-14 09:57:08 -0500

    Mute Slashified

--- src/commands/staff/mute.ts
+++ src/commands/staff/mute.ts
@@ -1,7 +1,7 @@
-import { staffPerms } from '@lib/permissions';
-import { ROLES } from '@root/config';
+import { ADMIN_PERMS, staffPerms, STAFF_PERMS } from '@lib/permissions';
+import { MAINTAINERS, ROLES } from '@root/config';
 import { userParser } from '@lib/arguments';
-import { Message, GuildMember } from 'discord.js';
+import { Message, GuildMember, ApplicationCommandPermissionData, CommandInteraction, ApplicationCommandOptionData } from 'discord.js';
 import { Command } from '@lib/types/Command';
 
 export default class extends Command {
@@ -9,6 +9,36 @@ export default class extends Command {
 	description = 'Gives the muted role to the given user.';
 	usage = '<user>';
 	runInDM = false;
+
+	tempPermissions: ApplicationCommandPermissionData[] = [STAFF_PERMS, ADMIN_PERMS];
+
+	options: ApplicationCommandOptionData[] = [
+		{
+			name: 'user',
+			description: 'The user to mute',
+			type: 'USER',
+			required: true
+		}
+	]
+
+	async tempRun(interaction: CommandInteraction): Promise<void> {
+		const user = interaction.options.getUser('user');
+		const member = await interaction.guild.members.fetch(user.id);
+
+		if (!member) {
+			interaction.reply({ content: `Something went wrong. Please contact ${MAINTAINERS} for help.`, ephemeral: true });
+			throw new Error('Could not find member based on passed in user');
+		}
+
+		if (member.roles.cache.has(ROLES.MUTED)) {
+			const reason = `${member.user.username} was un-muted by ${interaction.user.tag} (${interaction.user.id})`;
+			await member.roles.remove(ROLES.MUTED, reason);
+			return interaction.reply({ content: `${member.user.username} has been un-muted.`, ephemeral: true });
+		}
+		const reason = `${member.user.username} was muted by ${interaction.user.tag} (${interaction.user.id})`;
+		await member.roles.add(ROLES.MUTED, reason);
+		return interaction.reply({ content: `${member.user.username} has been muted.`, ephemeral: true });
+	}
 
 	permissions(msg: Message): boolean {
 		return staffPerms(msg);
