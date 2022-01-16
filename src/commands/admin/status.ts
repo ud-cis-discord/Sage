db7d5e000c74268516328231719cba02fde02f33
Ren Ross <briross@udel.edu>
2022-01-16 18:44:50 -0500

    removec, status, and sudo in progress tempperms and temprun have been created commands currently bugged

--- src/commands/admin/status.ts
+++ src/commands/admin/status.ts
@@ -1,15 +1,32 @@
 import { BOT } from '@root/config';
-import { botMasterPerms } from '@lib/permissions';
-import { Message } from 'discord.js';
+import { BOTMASTER_PERMS } from '@lib/permissions';
+import { ApplicationCommandOptionData, ApplicationCommandPermissionData, CommandInteraction, Message } from 'discord.js';
 import { Command } from '@lib/types/Command';
 
+const args = ['online', 'idle', 'dnd', 'invisible'];
 export default class extends Command {
 
 	description = `Sets ${BOT.NAME}'s status.`;
 	usage = '<online|idle|dnd|invisible>';
+	tempPermissions: ApplicationCommandPermissionData[] = [BOTMASTER_PERMS];
 
-	async permissions(msg: Message): Promise<boolean> {
-		return await botMasterPerms(msg);
+	options: ApplicationCommandOptionData[] = [{
+		name: 'status',
+		description: 'The status to give the bot (online, idle, dnd, invis).',
+		type: 'STRING',
+		required: true,
+		choices: args.map((arg) => ({
+			name: arg,
+			value: arg
+		}))
+	}]
+
+	async tempRun(interaction: CommandInteraction): Promise<void> {
+		const status = interaction.options.getString('status');
+		const bot = interaction.client;
+		//	bot.user.setStatus(status);
+
+		return interaction.reply(`Set ${BOT.NAME}'s status to ${status}`);
 	}
 
 	async run(msg: Message, [status]: ['online' | 'idle' | 'dnd' | 'invisible']): Promise<Message> {
