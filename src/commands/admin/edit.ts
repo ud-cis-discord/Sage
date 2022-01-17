6e096c2c1582ad27aa289a90b56f6c461baa8cbe
Ren Ross <briross@udel.edu>
2022-01-16 23:21:01 -0500

    edit.ts slashified

--- src/commands/admin/edit.ts
+++ src/commands/admin/edit.ts
@@ -1,5 +1,5 @@
-import { botMasterPerms } from '@lib/permissions';
-import { Message, TextChannel } from 'discord.js';
+import { BOTMASTER_PERMS } from '@lib/permissions';
+import { ApplicationCommandOptionData, ApplicationCommandPermissionData, CommandInteraction, Message, TextChannel } from 'discord.js';
 import { BOT } from '@root/config';
 import { Command } from '@lib/types/Command';
 
@@ -7,9 +7,41 @@ export default class extends Command {
 
 	description = `Edits a message sent by ${BOT.NAME}.`;
 	usage = '<messageLink>|<content>';
+	tempPermissions: ApplicationCommandPermissionData[] = [BOTMASTER_PERMS];
 
-	async permissions(msg: Message): Promise<boolean> {
-		return await botMasterPerms(msg);
+	options: ApplicationCommandOptionData[] = [{
+		name: 'msg_link',
+		description: 'A message link.',
+		type: 'STRING',
+		required: true
+	},
+	{
+		name: 'msg_content',
+		description: 'The updated message content.',
+		type: 'STRING',
+		required: true
+	}]
+
+	async tempRun(interaction: CommandInteraction): Promise<void> {
+		const link = interaction.options.getString('msg_link');
+		const content = interaction.options.getString('msg_content');
+
+		//	for discord canary users, links are different
+		const newLink = link.replace('canary.', '');
+		const match = newLink.match(/https:\/\/discord\.com\/channels\/(\d+)\/(\d+)\/(\d+)/);
+		if (!match) return interaction.reply('Please provide a valid message link.');
+
+		//	find the message
+		const [,, channelID, messageID] = match;
+		const message = await interaction.client.channels.fetch(channelID)
+			.then((channel: TextChannel) => channel.messages.fetch(messageID))
+			.catch(() => { throw 'I can\'t seem to find that message'; });
+
+		// check if the message can be edited
+		if (!message.editable) throw 'It seems I can\'t edit that message.';
+
+		await message.edit(content);
+		return interaction.reply('I\'ve updated that message.');
 	}
 
 	async run(msg: Message, [message, content]: [Message, string]): Promise<Message> {
