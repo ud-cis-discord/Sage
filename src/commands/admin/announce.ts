ed64a2c01d2617fe2ae3727bf2ee53c7ac8a37ba
Ren Ross <briross@udel.edu>
2022-01-16 20:00:57 -0500

    announce.ts slashified takes in image url

--- src/commands/admin/announce.ts
+++ src/commands/admin/announce.ts
@@ -1,6 +1,6 @@
-import { botMasterPerms } from '@lib/permissions';
+import { BOTMASTER_PERMS } from '@lib/permissions';
 import { channelParser } from '@lib/arguments';
-import { TextChannel, Message } from 'discord.js';
+import { TextChannel, Message, ApplicationCommandPermissionData, CommandInteraction, ApplicationCommandOptionData } from 'discord.js';
 import { CHANNELS } from '@root/config';
 import { Command } from '@lib/types/Command';
 
@@ -8,9 +8,36 @@ export default class extends Command {
 
 	description = 'Sends an announcement from Sage to a specified channel or announcements if no channel is given.';
 	usage = '[channel] | <content>';
+	tempPermissions: ApplicationCommandPermissionData[] = [BOTMASTER_PERMS];
 
-	async permissions(msg: Message): Promise<boolean> {
-		return await botMasterPerms(msg);
+	options: ApplicationCommandOptionData[] = [{
+		name: 'content',
+		description: 'announcement content',
+		type: 'STRING',
+		required: true
+	},
+	{
+		name: 'image',
+		description: 'announcement image url',
+		type: 'STRING',
+		required: false
+	}]
+
+	async tempRun(interaction: CommandInteraction): Promise<void> {
+		const channel = interaction.guild.channels.cache.get(CHANNELS.ANNOUNCEMENTS) as TextChannel;
+		const content = interaction.options.getString('content');
+		const image = interaction.options.getString('image');
+		await channel.send({
+			content: content,
+			allowedMentions: { parse: ['everyone', 'roles'] }
+		});
+		if (image) {
+			await channel.send({
+				content: image
+			});
+		}
+
+		return interaction.reply(`Your announcement has been sent in ${channel}`);
 	}
 
 	async run(msg: Message, [channel, content]: [TextChannel, string]): Promise<Message> {
