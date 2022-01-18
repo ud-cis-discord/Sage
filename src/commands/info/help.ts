0fa112f0e6979d96df1b2171e39db758f2ad1342
Blade Tyrrell <bladetyr@udel.edu>
2022-01-18 15:37:11 -0500

    help slashified

--- src/commands/info/help.ts
+++ src/commands/info/help.ts
@@ -1,6 +1,6 @@
-import { EmbedField, Message, MessageEmbed, Util } from 'discord.js';
+import { ApplicationCommandOptionData, CommandInteraction, EmbedField, MessageEmbed, TextChannel, Util } from 'discord.js';
 import { getCommand } from '@lib/utils';
-import { BOT, PREFIX } from '@root/config';
+import { BOT, CHANNELS, PREFIX } from '@root/config';
 import { Command } from '@lib/types/Command';
 
 export default class extends Command {
@@ -8,14 +8,27 @@ export default class extends Command {
 	description = `Provides info about all ${BOT.NAME} commands`;
 	usage = '[command]';
 	extendedHelp = 'If given no arguments, a list of all commands you have access to will be sent to your DMs';
-	aliases = ['commands', 'man', 'h'];
 
-	run(msg: Message, [cmd]: [string]): Promise<Message | void> {
-		const { commands } = msg.client;
+	options: ApplicationCommandOptionData[] = [
+		{
+			name: 'cmd',
+			description: 'command you would like to know more about',
+			type: 'STRING',
+			required: false
+		}
+	]
+
+	async tempRun(interaction: CommandInteraction): Promise<void> {
+		const cmd = interaction.options.getString('cmd');
+		const { commands } = interaction.client;
 		const website = 'https://ud-cis-discord.github.io/pages/commands';
+		const channel = await interaction.guild.channels.fetch(CHANNELS.SAGE) as TextChannel;
+
 		if (cmd) {
-			const command = getCommand(msg.client, cmd);
-			if (!command) return msg.channel.send(`**${cmd}** is not a valid command.`);
+			const command = getCommand(interaction.client, cmd);
+			if (!command) {
+				return interaction.reply(`**${cmd}** is not a valid command.`);
+			}
 
 			const fields: Array<EmbedField> = [];
 			fields.push({
@@ -23,13 +36,7 @@ export default class extends Command {
 				value: `${PREFIX}${command.name} ${command.usage ? command.usage : ''}`,
 				inline: true
 			});
-			if (command.aliases) {
-				fields.push({
-					name: 'Aliases',
-					value: command.aliases.join(', '),
-					inline: true
-				});
-			}
+
 			if (command.extendedHelp) {
 				fields.push({
 					name: 'Extended Help',
@@ -37,6 +44,7 @@ export default class extends Command {
 					inline: false
 				});
 			}
+
 			fields.push({
 				name: 'More commands',
 				value: `[Visit our website!](${website})`,
@@ -47,13 +55,15 @@ export default class extends Command {
 				.setTitle(command.name)
 				.setDescription(command.description ? command.description : '')
 				.addFields(fields)
-				.setThumbnail(msg.client.user.avatarURL())
+				.setThumbnail(interaction.client.user.avatarURL())
 				.setTimestamp(Date.now())
 				.setColor('RANDOM');
 
-			return msg.channel.send({ embeds: [embed] });
+			channel.send({ embeds: [embed] });
+			return interaction.reply({ content: 'Help sent!', ephemeral: true });
 		} else {
-			let helpStr = `You can do \`${PREFIX}help <command>\` to get more information about any command, or you can visit our website here:\n<${website}>\n`;
+			// if no command given
+			let helpStr = `You can do \`/help <command>\` to get more information about any command, or you can visit our website here:\n<${website}>\n`;
 			const categories: Array<string> = [];
 			commands.forEach(command => {
 				if (!categories.includes(command.category)) categories.push(command.category);
@@ -62,8 +72,7 @@ export default class extends Command {
 			categories.forEach(cat => {
 				const useableCmds = commands.filter(command =>
 					command.category === cat
-					&& !(command.permissions && !command.permissions(msg))
-					&& command.enabled !== false);
+					&& (command.enabled !== false));
 				const categoryName = cat === 'commands' ? 'General' : `${cat[0].toUpperCase()}${cat.slice(1)}`;
 				if (useableCmds.size > 0) {
 					helpStr += `\n**${categoryName} Commands**\n`;
@@ -75,23 +84,23 @@ export default class extends Command {
 
 			const splitStr = Util.splitMessage(helpStr, { char: '\n' });
 
-			let notified = false;
+			let part = 1;
 			splitStr.forEach(helpMsg => {
-				msg.author.send(helpMsg)
-					.then(() => {
-						if (!notified) {
-							if (msg.channel.type !== 'DM') msg.channel.send('I\'ve sent all commands to your DMs');
-							notified = true;
-						}
-					})
-					.catch(() => {
-						if (!notified) {
-							msg.channel.send('I couldn\'t send you a DM. Please enable DMs and try again');
-							notified = true;
-						}
-					});
+				const embed = new MessageEmbed()
+					.setTitle(`Commands Part ${part}`)
+					.setDescription(helpMsg)
+					.setColor('RANDOM');
+				interaction.user.send({ embeds: [embed] });
+				part++;
+				if (part === 3) {
+					interaction.reply({ content: 'Help sent!', ephemeral: true });
+					return;
+				}
 			});
 		}
+	}
+	async run(): Promise<void> {
+		return;
 	}
 
 }
