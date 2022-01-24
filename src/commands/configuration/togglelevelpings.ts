902aaffdaa6b73332de7ad01221ea86938d0bbd3
Blade Tyrrell <bladetyr@udel.edu>
2022-01-24 17:22:21 -0500

    togglelevelpings slashified

--- src/commands/configuration/togglelevelpings.ts
+++ src/commands/configuration/togglelevelpings.ts
@@ -1,26 +1,27 @@
 import { DB } from '@root/config';
 import { SageUser } from '@lib/types/SageUser';
-import { Message } from 'discord.js';
+import { CommandInteraction } from 'discord.js';
 import { DatabaseError } from '@lib/types/errors';
 import { Command } from '@lib/types/Command';
 
 export default class extends Command {
 
 	description = `Toggles whether or not you will receive notifications from Sage on a level up.`;
-	aliases = ['levelpings', 'lp'];
 
-	async run(msg: Message): Promise<Message> {
-		const entry: SageUser = await msg.client.mongo.collection(DB.USERS).findOne({ discordId: msg.author.id });
+	async tempRun(interaction: CommandInteraction): Promise<void> {
+		const entry: SageUser = await interaction.client.mongo.collection(DB.USERS).findOne({ discordId: interaction.user.id });
 
 		if (!entry) {
-			throw new DatabaseError(`Member ${msg.author.username} (${msg.author.id}) not in database`);
+			throw new DatabaseError(`Member ${interaction.user.username} (${interaction.user.id}) not in database`);
 		}
 
 		entry.levelPings = !entry.levelPings;
+		interaction.client.mongo.collection(DB.USERS).updateOne({ discordId: interaction.user.id }, { $set: { levelPings: entry.levelPings } });
+		return interaction.reply({ content: `You will${entry.levelPings ? ' now' : ' no longer'} receive notifications from Sage on a level up.`, ephemeral: true });
+	}
 
-		msg.client.mongo.collection(DB.USERS).updateOne({ discordId: msg.author.id }, { $set: { levelPings: entry.levelPings } });
-
-		return msg.channel.send(`You will${entry.levelPings ? ' now' : ' no longer'} receive notifications from Sage on a level up.`);
+	async run(): Promise<void> {
+		return;
 	}
 
 }
