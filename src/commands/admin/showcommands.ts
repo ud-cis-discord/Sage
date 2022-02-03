27a6fef1406d43b0e006f6bf74baa0729b9c66c1
Ren Ross <briross@udel.edu>
2022-02-02 22:11:05 -0500

    removed restrictCmds code

--- src/commands/admin/showcommands.ts
+++ src/commands/admin/showcommands.ts
@@ -7,25 +7,7 @@ export default class extends Command {
 	description = 'Show all commands, including disable commands.';
 	tempPermissions: ApplicationCommandPermissionData[] = [BOTMASTER_PERMS];
 
-	options: ApplicationCommandOptionData[] = [{
-		name: 'restricted',
-		description: 'Use this argument to see the list of restricted commands.',
-		type: 'BOOLEAN',
-		required: false
-	}]
-
 	async tempRun(interaction: CommandInteraction): Promise<void> {
-		const restricted = interaction.options.getBoolean('restricted');
-		if (restricted) {
-			/*
-			let commands = '[ Restricted ]\n.Unrestricted\n';
-			interaction.client.commands.forEach(command => {
-				commands += `\n${command.restricted === true ? `[ ${command.name} ]` : `.${command.name}`} `;
-			});
-			return interaction.reply(Formatters.codeBlock('css', commands));
-			*/
-			return interaction.reply('restricted works');
-		}
 		let commands = '+ Enabled\n- Disabled\n';
 
 		interaction.client.commands.forEach(command => {
