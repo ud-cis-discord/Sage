e534897fec4b852bb50d23e5ab40a6124c28b057
Ren Ross <briross@udel.edu>
2022-02-03 15:55:35 -0500

    made replies ephemeral

--- src/commands/admin/edit.ts
+++ src/commands/admin/edit.ts
@@ -38,8 +38,11 @@ export default class extends Command {
 			.catch(() => { throw 'I can\'t seem to find that message'; });
 
 		// check if the message can be edited
-		if (!message.editable) throw `It seems I can't edit that message. You'll need to tag a message that was sent by me, ${BOT.NAME}`;
-
+		if (!message.editable) {
+			return interaction.reply(
+				{ content: `It seems I can't edit that message. You'll need to tag a message that was sent by me, ${BOT.NAME}`,
+					ephemeral: true });
+		}
 		await message.edit(content);
 		return interaction.reply('I\'ve updated that message.');
 	}
