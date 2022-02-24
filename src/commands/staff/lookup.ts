ac818d21512442b4c3bb13fea0e3c1e3018f3aa2
renross <briross@udel.edu>
2022-02-23 20:30:44 -0500

    lookup command now sends info to intended user lookup embed styling changed to resemble check embed lookup email re-formatted into table

--- src/commands/staff/lookup.ts
+++ src/commands/staff/lookup.ts
@@ -22,39 +22,37 @@ export default class extends Command {
 	async run(interaction: CommandInteraction): Promise<void> {
 		const user = interaction.options.getUser('user');
 		const entry: SageUser = await interaction.client.mongo.collection(DB.USERS).findOne({ discordId: user.id });
+		const member = await interaction.guild.members.fetch(user.id);
 
 		if (!entry) {
 			return interaction.reply({ content: `User ${user.tag} has not verified.`, ephemeral: true });
 		}
 
 		const embed = new MessageEmbed()
+			.setTitle(`Looking Up:	${member.displayName}`)
+			.setThumbnail(user.avatarURL())
 			.setColor('GREEN')
-			.setAuthor(user.username, user.avatarURL())
-			.setFooter(`ID: ${user.id}`)
+			.setFooter(`Member ID: ${user.id}`)
+			.setTimestamp()
 			.addFields([
-				{
-					name: 'Email:',
-					value: entry.email,
-					inline: true
-				},
-				{
-					name: 'Messages: ',
-					value: entry.count.toString(),
-					inline: true
-				}
+				{ name: 'Display Name', value: `<@${member.id}>`, inline: true },
+				{ name: 'Username', value: `${user.tag}`, inline: false },
+				{ name: 'UD Email:', value: entry.email, inline: false },
+				{ name: 'Message count: ', value: `This week: ${entry.count.toString()}`, inline: false }
 			]);
 
 		if (!entry.pii) {
-			const sender: SageUser = await interaction.client.mongo.collection(DB.USERS).findOne({ discordId: user.id });
-			this.sendEmail(sender.email, user.username, entry);
-			return interaction.reply({ content: `That user has not opted in to have their information shared over Discord. 
-	An email has been sent to you containing the requested data.`, ephemeral: true });
+			const sender: SageUser = await interaction.client.mongo.collection(DB.USERS).findOne({ discordId: interaction.user.id });
+			this.sendEmail(sender.email, member.displayName, user.tag, entry);
+			return interaction.reply(
+				{ content: `\`${user.tag}\` has not opted to have their information shared over Discord.\nInstead, an email has been sent to you containing the requested data.`,
+					ephemeral: true });
 		}
 
 		return interaction.reply({ embeds: [embed], ephemeral: true });
 	}
 
-	sendEmail(recipient: string, username: string, entry: SageUser): void {
+	sendEmail(recipient: string, displayName: string, username: string, entry: SageUser): void {
 		const mailer = nodemailer.createTransport({
 			host: 'mail.udel.edu',
 			port: 25
@@ -64,26 +62,28 @@ export default class extends Command {
 			from: EMAIL.SENDER,
 			replyTo: EMAIL.REPLY_TO,
 			to: recipient,
-			subject: `Requested student information`,
+			subject: `UD CIS Discord:requested student information`,
 			html: `<!DOCTYPE html>
-	<html>
-
-	<head>
-		<title>User Information</title>
-	</head>
-
-	<body>
-
-		<h1>Your requested user information: </h1>
-		<p>User: ${username}</p>
-		<p>Email: ${entry.email}</p>
-		<p>Message Count: ${entry.count}</p>
-		<p>ID: ${entry.discordId}</p>
-		<p><br>Thanks for using Sage!</p>
-
-	</body>
-
-	</html>`
+			<html>
+				<body>
+					<h4>Your requested user information:</h4>
+					<table style="text-align: left; border: 1px solid black; border-collapse: collapse;">
+						<tr>
+							<th style="padding-right: 50px; border: 1px solid black;">Display Name</th>
+							<th style="padding-right: 50px; border: 1px solid black;">Username</th>
+							<th style="padding-right: 50px; border: 1px solid black;">University Email</th>
+							<th style="padding-right: 50px; border: 1px solid black;">Message Count</th>
+							<th style="padding-right: 50px; border: 1px solid black;">Member ID</th></tr>
+						<tr>
+							<td style="border: 1px solid black;">${displayName}</td>
+							<td style="border: 1px solid black;">${username}</td>
+							<td style="border: 1px solid black;">${entry.email}</td>
+							<td style="border: 1px solid black;">This week: ${entry.count}</td>
+							<td style="border: 1px solid black;">${entry.discordId}</td></tr>
+					</table>
+					<p><br>Thank you for using the UD CIS Discord Server and Sage!</p>
+				</body>
+			</html>`
 		});
 	}
 
